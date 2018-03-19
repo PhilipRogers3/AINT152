45e929637de52c0ac538b0881b046bb160ea86ed
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-03-19 13:19:00 +0000

    4

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game"); // change this to the level 1 
+    }
+    
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("JAZZ");
+    }
+}
--- Assets/Scripts/ZombieBehaviour.cs
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class ZombieBehaviour : MonoBehaviour {
 
     public int health = 10;
+    public int damage = 2;
     public GameObject explosionPrefab;
     public float adjustExplosionAngle = 0.0f;
-	
+
+    private Transform player;
+
+    private void Start()
+    {
+        if (GameObject.FindWithTag("Player"))
+        {
+            player = GameObject.FindWithTag("Player").transform;
+
+            GetComponent<MoveTowardsObject>().target = player;
+            GetComponent<SmoothLookAtTarget2D>().target = player;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SendMessage("TakeDamage", damage);
+        }
+    }
+
     public void TakeDamage( int damage)
     {
         health -= damage;
@@ -18,8 +40,14 @@ public class ZombieBehaviour : MonoBehaviour {
 
             GameObject explosion = Instantiate(explosionPrefab, transform.position, newRot);
             Destroy(explosion, 1);
-
+            GetComponent<AddScore>().DoSendScore();
             Destroy(gameObject);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
     }
 }
