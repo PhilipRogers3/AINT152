0ca2f2fbe4959cf27acb1d1fe9a4138af17d2a35
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-04-30 13:43:19 +0100

    Menu stuff
    Working on menus

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+
+	public void SetMusicVolume (float volume)
+    {
+        audioMixer.SetFloat("Music Volume", volume);
+    }
+
+    public void SetGameVolume(float volume)
+    {
+        audioMixer.SetFloat("Game Volume", volume);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+
+    }
+}
