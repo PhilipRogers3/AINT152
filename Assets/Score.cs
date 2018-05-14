cfac16bee345cfc67007eada0eaa04f9187ce67d
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-05-14 13:47:12 +0100

    more work on my game

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
@@ -9,6 +9,7 @@ public class Score : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		scoreText.text = 
+        scoreText.text = ("");
+
 	}
 }
