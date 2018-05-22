76670601827b48fa8a0c98b0586c9a687c2e924b
Philip Rogers (Rogue) <33717739+PhilipRogers3@users.noreply.github.com>
2018-05-22 20:30:19 +0100

    shop done
    coding and art done

---
+++ Assets/Scripts/ShopControlScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopControlScript : MonoBehaviour
+{
+
+    int moneyAmount;
+    int isGuitarSold;
+
+    public Text moneyAmountText;
+    public Text guitarPrice;
+
+    public Button buyButton; 
+
+
+	void Start ()
+    {
+        moneyAmount = PlayerPrefs.GetInt("MoneyAmount");
+	}
+	
+	
+	void Update ()
+    {
+        moneyAmountText.text = "Money" + moneyAmount.ToString() + "£";
+
+        isGuitarSold = PlayerPrefs.GetInt("isGuitarSold");
+
+        if (moneyAmount >= 25 && isGuitarSold == 0)
+            buyButton.interactable = true;
+        else
+            buyButton.interactable = false;
+	}
+
+    public void buyElectric()
+    {
+        moneyAmount -= 25;
+        PlayerPrefs.SetInt("isGuitarSold", 1);
+        guitarPrice.text = "Sold";
+        buyButton.gameObject.SetActive(false);
+    }
+
+    public void exitShop()
+    {
+        PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void resetPlayerPrefs()
+    {
+        moneyAmount = 0;
+        buyButton.gameObject.SetActive(true);
+        guitarPrice.text = "Price £25";
+        PlayerPrefs.DeleteAll();
+    }
+}
